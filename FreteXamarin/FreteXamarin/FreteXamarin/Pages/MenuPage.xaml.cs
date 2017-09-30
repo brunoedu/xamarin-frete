384140a1565dacc2fba71375ffff212168e6445c
Bruno Oliveira <bruno95edu@gmail.com>
2017-09-30 01:35:45 -0300

    Inicial

---
+++ FreteXamarin/FreteXamarin/FreteXamarin/Pages/MenuPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FreteXamarin.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPage : TabbedPage
+    {
+        public MenuPage()
+        {
+            Title = "Menu";
+
+            this.Children.Add(new HomePage
+            {
+                Title = "Home"
+            });
+            this.Children.Add(new CidadesPage
+            {
+                Title = "Cidades"
+            });
+            this.Children.Add(new FretePage
+            {
+                Title = "Frete"
+            });
+            InitializeComponent();
+        }
+    }
+}
